5d0b6613357d9bdbe4618f4bbb2b46de4bffd342
jordancoff <jordan.a.coff@gmail.com>
2020-09-08 18:42:45 -0400

    Get rid of storyboard and scene delegate.

--- BlankSwift/AppDelegate.swift
+++ BlankSwift/AppDelegate.swift
@@ -10,28 +10,15 @@ import UIKit
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
-
-
+  var window: UIWindow?
 
   func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-    // Override point for customization after application launch.
+    let mainWindow = UIWindow(frame: UIScreen.main.bounds)
+    let viewController = ViewController(nibName: nil, bundle: nil)
+    let navController = UINavigationController(rootViewController: viewController)
+    mainWindow.rootViewController = navController
+    mainWindow.makeKeyAndVisible()
+    window = mainWindow
     return true
   }
-
-  // MARK: UISceneSession Lifecycle
-
-  func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
-    // Called when a new scene session is being created.
-    // Use this method to select a configuration to create the new scene with.
-    return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
-  }
-
-  func application(_ application: UIApplication, didDiscardSceneSessions sceneSessions: Set<UISceneSession>) {
-    // Called when the user discards a scene session.
-    // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
-    // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
-  }
-
-
 }
-
